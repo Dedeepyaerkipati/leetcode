5a94c5f2f8a483e9eda970109af5f297a272f752
Dedeepyaerkipati <dedeepyaerkipati@gmail.com>
2026-09-07 20:21:02 +0530

    Time: 0 ms (100%), Space: 8.3 MB (52.48%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string interpret(string command) {
+      string ans;
+     for(int i=0;i<command.size();)
+     {
+        if(command[i]=='G')
+        {
+            ans+='G';
+            i++;
+        }
+        else if(command[i]=='(' && command[i+1]==')')
+        {
+            ans+='o';
+            i+=2;
+
+        }
+        else if(command[i]=='(' && command[i+1]=='a' && command[i+2]=='l' &&  command[i+3]==')')
+        {
+            ans+="al";
+
+            i+=4;
+        }
+
+     }  
+    return ans; 
+
+    }
+};
