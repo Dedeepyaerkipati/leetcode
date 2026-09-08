dae68b351c29ece884c03280e4bdd144666ef44e
Dedeepyaerkipati <dedeepyaerkipati@gmail.com>
2026-09-08 12:15:43 +0530

    Time: 0 ms (100%), Space: 12.2 MB (69.76%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+    
+    int l=0;
+
+       for(int i=0;i<=s.size();i++)
+       {
+        if( i==s.size() || s[i]==' ')
+        {
+            reverse(s.begin()+l,s.begin()+i);
+            l=i+1;
+        }
+       }
+       return s;
+    }
+};
